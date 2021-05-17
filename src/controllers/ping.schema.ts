dd9af4eba7e33bf7b4fa23d37774d5cbfe965e9d
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-05-17 20:12:51 +0700

    feat: update truck

--- src/controllers/ping.schema.ts
+++ src/controllers/ping.schema.ts
@@ -3,44 +3,6 @@ const whereSchema = {
   carrier_id: { type: "number" },
 }
 const pingSchema: FastifySchema = {
-  // params: {  // GET => require
-  //   type: 'object',
-  //   properties: {
-  //     par1: { type: 'string' },
-  //     par2: { type: 'number' }
-  //   }
-  // },
-  // querystring: { // GET => don't require
-  //   type: 'object',
-  //   properties: {
-  //     // filter: {
-  //     //   type: 'object',
-  //     //   // where: { type: "string" },
-  //     //   // test1: { type: 'string' }
-  //     //   // where: {
-  //     //   //   type: 'array',
-  //     //   //   items: {
-  //     //   //     carrier_id: { type: 'number' }
-  //     //   //   }
-  //     //   //   // anyOf: [
-  //     //   //   //   {
-  //     //   //   //     type: 'object',
-  //     //   //   //     properties: {
-  //     //   //   //       carrier_id: { type: 'number' }
-  //     //   //   //     },
-  //     //   //   //     require: ['carrier_id']
-  //     //   //   //   },
-  //     //   //   // ]
-  //     //   // }
-  //     // }
-  //     type: "array",
-  //     items: {
-  //       properties: whereSchema
-  //     }
-  //   }
-  // },
-
-
   querystring: {
     type: 'object',
     in: "query",
