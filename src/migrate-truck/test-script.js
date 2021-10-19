1a7a1bdcb3777f9c3905a3dc2232bf8c2a5f39d7
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-10-19 22:49:37 +0700

    chore: script

--- src/migrate-truck/test-script.js
+++ src/migrate-truck/test-script.js
@@ -37,19 +37,42 @@ const addFamilyColumn = async () => {
   const clientJobService = new Pool(devConnection)
   const newJobConnection = await clientJobService.connect();
 
-  const { rows: RowJob } = await newJobConnection.query(`SELECT * FROM job_clone_v3;`);
+  // const { rows: RowJob } = await newJobConnection.query(`SELECT * FROM job_clone_v3;`);
+
+  // ** ADD VIEW
+  const sqlCreateViewJobListV2 = `CREATE OR REPLACE VIEW vw_job_list_v2 AS  SELECT j.id,
+  j.user_id,
+  j.product_type_id,
+  j.product_name,
+  j.truck_type,
+  j.total_weight AS weight,
+  j.truck_amount AS required_truck_amount,
+  j.loading_address,
+  j.loading_datetime,
+  j.loading_contact_name,
+  j.loading_contact_phone,
+  j.loading_latitude,
+  j.loading_longitude,
+  j.status,
+  j.offered_total AS price,
+  j.price_type,
+  j.tipper,
+  j.is_deleted,
+  j.public_as_cgl,
+  json_agg(json_build_object('name', s.address_dest, 'dateTime', s.delivery_datetime, 'contactName', s.fullname_dest, 'contactMobileNo', s.phone_dest, 'lat', s.latitude_dest::character varying, 'lng', s.longitude_dest::character varying)) AS shipments,
+  j.full_text_search,
+  j.created_at,
+  j.family
+ FROM job j
+   LEFT JOIN shipment s ON s.job_id = j.id
+GROUP BY j.id, j.user_id, j.product_type_id, j.product_name, j.truck_type, j.total_weight, j.truck_amount, j.loading_address, j.loading_datetime, j.loading_contact_name, j.loading_contact_phone, j.loading_latitude, j.loading_longitude, j.status, j.offered_total, j.price_type, j.tipper, j.is_deleted, j.full_text_search, j.public_as_cgl, j.family, j.created_at;`
+  await newJobConnection.query(sqlCreateViewJobListV2);
 
   // ** ADD COLUMN
-  // const sqlAddColumn = `ALTER TABLE job_clone_v3
-  // ADD COLUMN family jsonb;`
-  // await newJobConnection.query(sqlAddColumn);
+  const sqlAddColumn = `ALTER TABLE job
+  ADD COLUMN family jsonb;`
+  await newJobConnection.query(sqlAddColumn);
 
-  // ** UPDATE NULL
-  // for (const attr of RowJob) {
-  //     await newJobConnection.query(`UPDATE job_clone_v3
-  //   SET family = null
-  //   WHERE id = ${attr.id}`);
-  // }
 
 
   console.log('Finished add job family Column');
