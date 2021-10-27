d9f6f1333d8b21056fc1fb21f9c4cf8a48be8432
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-10-27 17:26:09 +0700

    chore: update script

--- src/migrate-truck/test-script.js
+++ src/migrate-truck/test-script.js
@@ -1,18 +1,6 @@
 const { Pool } = require('pg');
-const sql = require('sql');
-const jwt = require('jsonwebtoken')
-const axios = require('axios')
-const Hashids = require('hashids');
-const _ = require('lodash');
-const { EEXIST } = require('constants');
-const { xor } = require('lodash');
-// const Utility = require('utility-layer/dist/security');
-// const util = new Utility();
 
 const addFamilyColumn = async () => {
-
-  // console.log("Mix array : ", mixArr)
-
   const devConnection = {
     host: "cgl-dev-db.ccyrpfjhgi1v.ap-southeast-1.rds.amazonaws.com",
     user: "postgres",
@@ -34,10 +22,15 @@ const addFamilyColumn = async () => {
     database: 'job_service',
     port: 5432,
   }
-  const clientJobService = new Pool(devConnection)
+  const clientJobService = new Pool(prodConnection)
   const newJobConnection = await clientJobService.connect();
 
   // const { rows: RowJob } = await newJobConnection.query(`SELECT * FROM job_clone_v3;`);
+
+  // ** ADD COLUMN
+  const sqlAddColumn = `ALTER TABLE job
+  ADD COLUMN family jsonb;`
+  await newJobConnection.query(sqlAddColumn);
 
   // ** ADD VIEW
   const sqlCreateViewJobListV2 = `CREATE OR REPLACE VIEW vw_job_list_v2 AS  SELECT j.id,
@@ -67,13 +60,6 @@ const addFamilyColumn = async () => {
    LEFT JOIN shipment s ON s.job_id = j.id
 GROUP BY j.id, j.user_id, j.product_type_id, j.product_name, j.truck_type, j.total_weight, j.truck_amount, j.loading_address, j.loading_datetime, j.loading_contact_name, j.loading_contact_phone, j.loading_latitude, j.loading_longitude, j.status, j.offered_total, j.price_type, j.tipper, j.is_deleted, j.full_text_search, j.public_as_cgl, j.family, j.created_at;`
   await newJobConnection.query(sqlCreateViewJobListV2);
-
-  // ** ADD COLUMN
-  const sqlAddColumn = `ALTER TABLE job
-  ADD COLUMN family jsonb;`
-  await newJobConnection.query(sqlAddColumn);
-
-
 
   console.log('Finished add job family Column');
   return true;
